0e5e8b649bb40481232f1b187c14fe0a9d89054e
Alexandr Boico <boico.alexandr@isa.utm.md>
2022-06-01 16:26:10 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+
+//TO DO:
+/*
+    1)  wifi shield esp8266 connection  (hard)
+
+    2)  Climate control                 (easy)
+        a)  DHT11 sensor          
+        b)  fan control 
+        c)  heater element control
+
+    3)  RFID lock                       (middle)
+        a)  RFID panel
+        b)  electromagnetic lock
+
+    4)  Water leakage                   (easy)
+        a)  water detector
+        b)  servo
+
+    5)  Gas leakage                     (easy)
+        a)  gas detector
+        b)  servo
+*/
+
+//TO BUY LIST:
+/*
+    1)  sourse of power
+    2)  L298 (or other) motor driver
+    3)  electromagnetic lock(solenoid)
+    
+*/
